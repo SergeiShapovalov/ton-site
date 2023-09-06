696b85644d44cfa81a06e3ade646ccb58355dc94
pakkonen <pakkonen-s@yandex.ru>
2023-09-06 20:48:23 +0300

    Update install_packets_for_rldp_proxy.sh

--- install_packets_for_rldp_proxy.sh
+++ install_packets_for_rldp_proxy.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-apt-get update && apt-get upgrade -y
-apt-get install -y nginx
-apt-get clean all
-apt-get install pkg-config
-apt-get install -y systemd
 
-# echo "daemon off;" >> /etc/nginx/nginx.conf
-rm /var/www/html/index.nginx-debian.html
-cp html_ton/index.html /var/www/html/index.nginx-debian.html
-cp html_ton/qr_ton.jpg /var/www/html/
 
 # Создаём раздел подкачки (иначе не на всех серверах запустится tonlib-cli)
 mkdir -p /var/cache/swap/
